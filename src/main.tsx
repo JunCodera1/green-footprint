357445f8290099852513eaa35bc249c95aad7125
JunCodera1 <minhtien0601200@gmail.com>
2025-05-27 11:06:35 +0700

    Removed: index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import GreenFootprintLanding from "./pages/GreenFootprintLanding";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CarbonFootprintCalculator from "./pages/CarbonFootprintCalculator";
