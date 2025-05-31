3ef30ffa25c0ed5b7735f17964147de19108b33f
JunCodera1 <minhtien0601200@gmail.com>
2025-05-31 14:03:05 +0700

    Added: types for some components threejs

---
+++ src/types/three-jsm.d.ts
@@ -0,0 +1,69 @@
+// src/types/three-jsm.d.ts
+declare module "three/examples/jsm/loaders/GLTFLoader" {
+  import { Loader, LoadingManager, Group } from "three";
+
+  export class GLTFLoader extends Loader {
+    constructor(manager?: LoadingManager);
+    load(
+      url: string,
+      onLoad: (gltf: any) => void,
+      onProgress?: (event: ProgressEvent) => void,
+      onError?: (event: ErrorEvent) => void
+    ): void;
+  }
+}
+
+declare module "three/examples/jsm/controls/OrbitControls" {
+  import {
+    Camera,
+    EventDispatcher,
+    MOUSE,
+    Object3D,
+    Raycaster,
+    Spherical,
+    TOUCH,
+    Vector3,
+  } from "three";
+
+  export class OrbitControls extends EventDispatcher {
+    constructor(object: Camera, domElement?: HTMLElement);
+
+    object: Camera;
+    domElement: HTMLElement | HTMLDocument;
+
+    enabled: boolean;
+    target: Vector3;
+    minDistance: number;
+    maxDistance: number;
+    minZoom: number;
+    maxZoom: number;
+    minPolarAngle: number;
+    maxPolarAngle: number;
+    minAzimuthAngle: number;
+    maxAzimuthAngle: number;
+    enableDamping: boolean;
+    dampingFactor: number;
+    enableZoom: boolean;
+    zoomSpeed: number;
+    enableRotate: boolean;
+    rotateSpeed: number;
+    enablePan: boolean;
+    panSpeed: number;
+    screenSpacePanning: boolean;
+    keyPanSpeed: number;
+    autoRotate: boolean;
+    autoRotateSpeed: number;
+    enableKeys: boolean;
+    keys: { LEFT: string; UP: string; RIGHT: string; BOTTOM: string };
+    mouseButtons: { LEFT: MOUSE; MIDDLE: MOUSE; RIGHT: MOUSE };
+    touches: { ONE: TOUCH; TWO: TOUCH };
+
+    saveState(): void;
+    reset(): void;
+    update(): boolean;
+    dispose(): void;
+    getPolarAngle(): number;
+    getAzimuthalAngle(): number;
+    listenToKeyEvents(domElement: HTMLElement): void;
+  }
+}
