0da7abda9a115326ac6677d6d4e1d3b7f1d6546e
MichaelMinh <Michael@192.168.1.8.non-exists.ptr.local>
2025-06-07 23:59:27 +0700

    Fix Marketplace

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import React, { lazy, Suspense } from "react";
 
 import GreenFootprintLanding from "./pages/Eco/GreenFootprintLanding";
 import Blog from "./pages/Blog/Blog";
-import Community from "./pages/Support/Community";
+import Community from "./pages/Info/Community";
 import AIAnalytic from "./pages/API/AIAnalytic";
 import PersonalReports from "./pages/Support/PersonalReports";
 import CarbonFootprintCalculator from "./pages/Carbon/CarbonFootprintCalculator";
